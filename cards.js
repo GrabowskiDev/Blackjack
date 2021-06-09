7cd437b244a90fdb037207b69bd043d96c963684
GrabowskiDev <lukaszgra2004@gmail.com>
2021-06-09 22:40:57 +0200

    Changed png to svg

--- cards.js
+++ cards.js
@@ -3,271 +3,271 @@ const cards = [
 	{
 		mark: 'c',
 		value: 2,
-		path: './cards/clubs2.png',
+		path: './cards/clubs2.svg',
 	},
 	{
 		mark: 'c',
 		value: 3,
-		path: './cards/clubs3.png',
+		path: './cards/clubs3.svg',
 	},
 	{
 		mark: 'c',
 		value: 4,
-		path: './cards/clubs4.png',
+		path: './cards/clubs4.svg',
 	},
 	{
 		mark: 'c',
 		value: 5,
-		path: './cards/clubs5.png',
+		path: './cards/clubs5.svg',
 	},
 	{
 		mark: 'c',
 		value: 6,
-		path: './cards/clubs6.png',
+		path: './cards/clubs6.svg',
 	},
 	{
 		mark: 'c',
 		value: 7,
-		path: './cards/clubs7.png',
+		path: './cards/clubs7.svg',
 	},
 	{
 		mark: 'c',
 		value: 8,
-		path: './cards/clubs8.png',
+		path: './cards/clubs8.svg',
 	},
 	{
 		mark: 'c',
 		value: 9,
-		path: './cards/clubs9.png',
+		path: './cards/clubs9.svg',
 	},
 	{
 		mark: 'c',
 		value: 10,
-		path: './cards/clubs10.png',
+		path: './cards/clubs10.svg',
 	},
 	{
 		mark: 'c',
 		value: 10,
-		path: './cards/clubsJack.png',
+		path: './cards/clubsJack.svg',
 	},
 	{
 		mark: 'c',
 		value: 10,
-		path: './cards/clubsQueen.png',
+		path: './cards/clubsQueen.svg',
 	},
 	{
 		mark: 'c',
 		value: 10,
-		path: './cards/clubsKing.png',
+		path: './cards/clubsKing.svg',
 	},
 	{
 		mark: 'c',
 		value: 11,
-		path: './cards/clubsAce.png',
+		path: './cards/clubsAce.svg',
 	},
 
 	//Diamonds
 	{
 		mark: 'd',
 		value: 2,
-		path: './cards/diamonds2.png',
+		path: './cards/diamonds2.svg',
 	},
 	{
 		mark: 'd',
 		value: 3,
-		path: './cards/diamonds3.png',
+		path: './cards/diamonds3.svg',
 	},
 	{
 		mark: 'd',
 		value: 4,
-		path: './cards/diamonds4.png',
+		path: './cards/diamonds4.svg',
 	},
 	{
 		mark: 'd',
 		value: 5,
-		path: './cards/diamonds5.png',
+		path: './cards/diamonds5.svg',
 	},
 	{
 		mark: 'd',
 		value: 6,
-		path: './cards/diamonds6.png',
+		path: './cards/diamonds6.svg',
 	},
 	{
 		mark: 'd',
 		value: 7,
-		path: './cards/diamonds7.png',
+		path: './cards/diamonds7.svg',
 	},
 	{
 		mark: 'd',
 		value: 8,
-		path: './cards/diamonds8.png',
+		path: './cards/diamonds8.svg',
 	},
 	{
 		mark: 'd',
 		value: 9,
-		path: './cards/diamonds9.png',
+		path: './cards/diamonds9.svg',
 	},
 	{
 		mark: 'd',
 		value: 10,
-		path: './cards/diamonds10.png',
+		path: './cards/diamonds10.svg',
 	},
 	{
 		mark: 'd',
 		value: 10,
-		path: './cards/diamondsJack.png',
+		path: './cards/diamondsJack.svg',
 	},
 	{
 		mark: 'd',
 		value: 10,
-		path: './cards/diamondsQueen.png',
+		path: './cards/diamondsQueen.svg',
 	},
 	{
 		mark: 'd',
 		value: 10,
-		path: './cards/diamondsKing.png',
+		path: './cards/diamondsKing.svg',
 	},
 	{
 		mark: 'd',
 		value: 11,
-		path: './cards/diamondsAce.png',
+		path: './cards/diamondsAce.svg',
 	},
 
 	//Hearts
 	{
 		mark: 'h',
 		value: 2,
-		path: './cards/hearts2.png',
+		path: './cards/hearts2.svg',
 	},
 	{
 		mark: 'h',
 		value: 3,
-		path: './cards/hearts3.png',
+		path: './cards/hearts3.svg',
 	},
 	{
 		mark: 'h',
 		value: 4,
-		path: './cards/hearts4.png',
+		path: './cards/hearts4.svg',
 	},
 	{
 		mark: 'h',
 		value: 5,
-		path: './cards/hearts5.png',
+		path: './cards/hearts5.svg',
 	},
 	{
 		mark: 'h',
 		value: 6,
-		path: './cards/hearts6.png',
+		path: './cards/hearts6.svg',
 	},
 	{
 		mark: 'h',
 		value: 7,
-		path: './cards/hearts7.png',
+		path: './cards/hearts7.svg',
 	},
 	{
 		mark: 'h',
 		value: 8,
-		path: './cards/hearts8.png',
+		path: './cards/hearts8.svg',
 	},
 	{
 		mark: 'h',
 		value: 9,
-		path: './cards/hearts9.png',
+		path: './cards/hearts9.svg',
 	},
 	{
 		mark: 'h',
 		value: 10,
-		path: './cards/hearts10.png',
+		path: './cards/hearts10.svg',
 	},
 	{
 		mark: 'h',
 		value: 10,
-		path: './cards/heartsJack.png',
+		path: './cards/heartsJack.svg',
 	},
 	{
 		mark: 'h',
 		value: 10,
-		path: './cards/heartsQueen.png',
+		path: './cards/heartsQueen.svg',
 	},
 	{
 		mark: 'h',
 		value: 10,
-		path: './cards/heartsKing.png',
+		path: './cards/heartsKing.svg',
 	},
 	{
 		mark: 'h',
 		value: 11,
-		path: './cards/heartsAce.png',
+		path: './cards/heartsAce.svg',
 	},
 
 	//Spades
 	{
 		mark: 's',
 		value: 2,
-		path: './cards/spades2.png',
+		path: './cards/spades2.svg',
 	},
 	{
 		mark: 's',
 		value: 3,
-		path: './cards/spades3.png',
+		path: './cards/spades3.svg',
 	},
 	{
 		mark: 's',
 		value: 4,
-		path: './cards/spades4.png',
+		path: './cards/spades4.svg',
 	},
 	{
 		mark: 's',
 		value: 5,
-		path: './cards/spades5.png',
+		path: './cards/spades5.svg',
 	},
 	{
 		mark: 's',
 		value: 6,
-		path: './cards/spades6.png',
+		path: './cards/spades6.svg',
 	},
 	{
 		mark: 's',
 		value: 7,
-		path: './cards/spades7.png',
+		path: './cards/spades7.svg',
 	},
 	{
 		mark: 's',
 		value: 8,
-		path: './cards/spades8.png',
+		path: './cards/spades8.svg',
 	},
 	{
 		mark: 's',
 		value: 9,
-		path: './cards/spades9.png',
+		path: './cards/spades9.svg',
 	},
 	{
 		mark: 's',
 		value: 10,
-		path: './cards/spades10.png',
+		path: './cards/spades10.svg',
 	},
 	{
 		mark: 's',
 		value: 10,
-		path: './cards/spadesJack.png',
+		path: './cards/spadesJack.svg',
 	},
 	{
 		mark: 's',
 		value: 10,
-		path: './cards/spadesQueen.png',
+		path: './cards/spadesQueen.svg',
 	},
 	{
 		mark: 's',
 		value: 10,
-		path: './cards/spadesKing.png',
+		path: './cards/spadesKing.svg',
 	},
 	{
 		mark: 's',
 		value: 11,
-		path: './cards/spadesAce.png',
+		path: './cards/spadesAce.svg',
 	},
 ];
 
-const cardsBack = { path: './cards/rewers.png' };
+const cardsBack = { path: './cards/rewers.svg' };
 
 export { cards, cardsBack };
